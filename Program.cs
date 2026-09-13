a8d728eb25b94f116c575fca8d5c169a39557393
Petro <petrotymchuck@gmail.com>
2026-09-13 11:47:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tymchuk_Petro_IPZ_32_Duplocate_Detector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
